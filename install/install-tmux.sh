d8a47bb6c6ed2d018cd4224e4c83646253e544f7
Darkø Tasevski <darko.tasevski@aspendental.com>
2024-01-21 10:48:28 +0100

    refactor: update tmux config

--- install/install-tmux.sh
+++ install/install-tmux.sh
@@ -5,16 +5,17 @@ if [ ! -d "$TMUXDIR" ]; then
 fi
 
 rm "$DOTFILES/tmux/tmux.conf" >/dev/null
-cp "$DOTFILES/tmux/tmuxconfig_template" "$DOTFILES/tmux/tmux.conf"
-sed -i "s|__TMUXDIR__|${TMUXDIR}|g" "$DOTFILES"/tmux/tmux.conf
-ln -sf "$DOTFILES/tmux/tmux.conf" "$TMUXDIR"
+# cp "$DOTFILES/tmux/tmuxconfig_template" "$DOTFILES/tmux/tmux.conf"
+# sed -i "s|__TMUXDIR__|${TMUXDIR}|g" "$DOTFILES"/tmux/tmux.conf
+ln -sf "$DOTFILES/tmux/.tmux/.tmux.conf" "$TMUXDIR/tmux.conf"
+ln -sf "$DOTFILES/tmux/tmux.conf.local" "$TMUXDIR/tmux.conf.local"
 
 git clone https://github.com/tmux-plugins/tpm "$TMUXDIR/plugins/tpm"
 
 tmux source "$TMUXDIR"/tmux.conf
 # Fix for tmp crashing because TMUX_PLUGIN_MANAGER_PATH is not defined
 # @see https://giters.com/Ynjxsjmh/dotfiles
-# export TMUX_PLUGIN_MANAGER_PATH="$TMUXDIR/plugins"
+export TMUX_PLUGIN_MANAGER_PATH="$TMUXDIR/plugins"
 "$TMUXDIR"/plugins/tpm/bin/install_plugins
 
 for file in "$DOTFILES"/tmux/*.proj; do
