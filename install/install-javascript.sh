ebc6854d243bbfee3d6c44d1eb0b46f0d07ebe0b
Darkø Tasevski <darktasevski@gmail.com>
2025-03-01 13:59:13 +0100

    refactor: update dotfiles

--- install/install-javascript.sh
+++ install/install-javascript.sh
@@ -2,9 +2,5 @@
 
 ln -sfv "$DOTFILES"/javascript/npmrc "$HOME"/.npmrc
 
-[ ! -f "$NPM_PATH"/bin/bars ] && npm i -g https://github.com/jez/bars.git
-
-[ ! -d "$NPM_PATH"/lib/node_modules/typescript ] && npm i -g typescript
-
 # curl -o- -L https://yarnpkg.com/install.sh | bash
 # curl https://get.volta.sh | bash
